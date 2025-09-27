3b16acf8260e40e891c93d75388269d792dd44b9
Vortex-oo <debjit.github@gmail.com>
2025-09-27 11:25:25 +0530

    refactor: update dependency versions to use caret syntax and add Navbar component

---
+++ frontend/src/app/components/Navbar.tsx
@@ -0,0 +1,30 @@
+'use client'
+import React from 'react'
+import Link from "next/link";
+
+const Navbar = () => {
+    return (
+        <div className='flex justify-between items-center px-8 py-4 absolute w-full text-white z-50 font-mono'>
+
+            <div>
+                <h1 className='text-2xl font-bold tracking-widest'>
+                    BLOCK CERTIFY
+                </h1>
+            </div>
+
+            <div className='flex items-center space-x-6'>
+                <Link href='/' className='font-bold border-4 border-teal-900 rounded-2xl cursor-pointer text-white  hover:bg-teal-900 hover:text-white py-2 px-6 hover:rounded-full transition-all duration-300  hover:scale-105 hover:border-white'>
+                    Home
+                </Link>
+                <Link href='/create/create-certificate' className='font-bold border-4 border-orange-400 rounded-2xl cursor-pointer text-white  hover:bg-orange-400 hover:text-black py-2 px-6 hover:rounded-full transition-all duration-300  hover:scale-105 hover:border-white'>
+                    Create
+                </Link>
+                <Link href='/create/verify-certificate' className='font-bold border-4 border-green-700 rounded-2xl cursor-pointer text-white  hover:bg-green-500 hover:text-black py-2 px-6 hover:rounded-full transition-all duration-300  hover:scale-105 hover:border-white'>
+                    Verify
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
