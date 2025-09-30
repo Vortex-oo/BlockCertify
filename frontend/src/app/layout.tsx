c0ac12c29591b0a53964e7bae1d26df652ff77b1
Vortex-oo <debjit.github@gmail.com>
2025-09-30 08:25:14 +0530

    feat: add favicon and title to RootLayout for improved branding

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -26,6 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" />
+        <title>Block Certify</title>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
